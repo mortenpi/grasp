7186b1c4452ee36ac2f91a6841464c36482db3ed
Morten Piibeleht <morten.piibeleht@gmail.com>
2019-11-17 13:38:31 +0100

    Update run.sh

--- .github/workflows/run.sh
+++ .github/workflows/run.sh
@@ -1,1 +1,8 @@
+echo "Repo info:"
 curl -X GET -H"authorization: Bearer ${GITHUB_TOKEN}" "https://api.github.com/repos/mortenpi/grasp"
+
+echo "List gh-pages builds:"
+curl -X GET -H"authorization: Bearer ${GITHUB_TOKEN}" "https://api.github.com/repos/mortenpi/grasp/pages/builds"
+
+echo "Trigger gh-pages build:"
+curl -X POST -H"authorization: Bearer ${GITHUB_TOKEN}" "https://api.github.com/repos/mortenpi/grasp/pages/builds"
